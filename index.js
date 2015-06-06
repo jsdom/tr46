b22c3e63e038c82b9e96aa1c274162cdec52e002
Sebastian Mayr <sebmaster16@gmail.com>
2015-06-06 15:04:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,188 @@
+"use strict";
+
+var punycode = require("punycode");
+var mappingTable = require("./lib/mapping_table.json");
+
+var PROCESSING_OPTIONS = {
+  TRANSITIONAL: 0,
+  NONTRANSITIONAL: 1
+};
+
+function findStatus(val) {
+  var start = 0;
+  var end = mappingTable.length - 1;
+  
+  while (start <= end) {
+    var mid = Math.floor((start + end) / 2);
+    
+    var target = mappingTable[mid];
+    if (target[0][0] <= val && target[0][1] >= val) {
+      return target;
+    } else if (target[0][0] > val) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  
+  return null;
+}
+
+var regexAstralSymbols = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+
+function countSymbols(string) {
+  return string
+    // replace every surrogate pair with a BMP symbol
+    .replace(regexAstralSymbols, '_')
+    // then get the length
+    .length;
+}
+
+function mapChars(domain_name, useSTD3, processing_option) {
+  var hasError = false;
+  var processed = "";
+  
+  var len = countSymbols(domain_name);
+  for (var i = 0; i < len; ++i) {
+    var codePoint = domain_name.codePointAt(i);
+    var status = findStatus(codePoint);
+    
+    switch (status[1]) {
+      case "disallowed":
+        hasError = true;
+        processed += String.fromCodePoint(codePoint);
+        break;
+      case "ignored":
+        break;
+      case "mapped":
+        processed += String.fromCodePoint(status[2]);
+        break;
+      case "deviation":
+        if (processing_option === PROCESSING_OPTIONS.TRANSITIONAL) {
+          processed += String.fromCodePoint(status[2]);
+        } else {
+          processed += String.fromCodePoint(codePoint);
+        }
+        break;
+      case "valid":
+        processed += String.fromCodePoint(codePoint);
+        break;
+      case "disallowed_STD3_mapped":
+        if (useSTD3) {
+          processed += String.fromCodePoint(status[2]);
+        } else {
+          hasError = true;
+          processed += String.fromCodePoint(codePoint);
+        }
+        break;
+      case "disallowed_STD3_valid":
+        if (!useSTD3) {
+          hasError = true;
+        }
+        
+        processed += String.fromCodePoint(codePoint);
+        break;
+    }
+  }
+  
+  return {
+    string: processed,
+    error: hasError
+  };
+}
+
+function validateLabel(label, processing_option) {
+  if (label.substr(0, 4) === "xn--") {
+    label = punycode.toUnicode(label.substr(4));
+    processing_option = PROCESSING_OPTIONS.NONTRANSITIONAL;
+  }
+  
+  var error = false;
+  
+  if (label.normalize("NFC") !== label ||
+      (label[3] === "-" && label[4] === "-") ||
+      label[0] === "-" || label[label.length - 1] === "-" ||
+      label.indexOf(".") !== -1
+      // TODO: Must not begin with combining mark
+      ) {
+    error = true;
+  }
+  
+  var len = countSymbols(label);
+  for (var i = 0; i < len; ++i) {
+    var status = findStatus(label.codePointAt(i));
+    if ((processing === PROCESSING_OPTIONS.TRANSITIONAL && status[1] !== "valid") ||
+        (processing === PROCESSING_OPTIONS.NONTRANSITIONAL &&
+         status[1] !== "valid" && status[1] !== "deviation")) {
+      error = true;
+      break;
+    }
+  }
+  
+  return {
+    label: label,
+    error: error
+  };
+}
+
+function processing(domain_name, useSTD3, processing_option) {
+  var result = mapChars(domain_name, useSTD3, processing_option);
+  result.string = result.string.normalize("NFC");
+  
+  var labels = result.string.split(".");
+  for (var i = 0; i < labels.length; ++i) {
+    try {
+      var validation = validateLabel(labels[i]);
+      labels[i] = validation.label;
+      result.error = result.error || validation.error;
+    } catch(e) {
+      result.error = true;
+    }
+  }
+  
+  return {
+    string: labels.join("."),
+    error: result.error
+  };
+}
+
+module.exports.toASCII = function(domain_name, useSTD3, processing_option, verifyDnsLength) {
+  var result = processing(domain_name, useSTD3, processing);
+  var labels = result.string.split(".");
+  labels = labels.map(function(l) {
+    try {
+      return punycode.toASCII(l);
+    } catch(e) {
+      result.error = true;
+      return l;
+    }
+  });
+  
+  if (verifyDnsLength) {
+    var total = labels.slice(0, labels.length - 1).join(".").length;
+    if (total.length > 253 || total.length === 0) {
+      result.error = true;
+    }
+    
+    for (var i=0; i < labels.length; ++i) {
+      if (labels.length > 63 || labels.length === 0) {
+        result.error = true;
+        break;
+      }
+    }
+  }
+  
+  if (result.error) return null;
+  return labels.join(".");
+};
+
+module.exports.toUnicode = function(domain_name, useSTD3) {
+  var result = processing(domain_name, useSTD3, PROCESSING_OPTIONS.NONTRANSITIONAL);
+  
+  return {
+    domain: result.string,
+    error: result.error
+  };
+};
+
+module.exports.PROCESSING_OPTIONS = PROCESSING_OPTIONS;
---
+++ scripts/generate_mapping_table.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var fs = require("fs");
+var request = require("request");
+
+request.get("http://www.unicode.org/Public/idna/7.0.0/IdnaMappingTable.txt", function(err, res, body) {
+  var lines = [];
+  
+  body.split("\n").forEach(function(l) {
+    l = l.split("#")[0]; // remove comments
+    var cells = l.split(";").map(function(c) {
+      return c.trim();
+    });
+    if (cells.length === 1) return;
+    
+    // parse ranges to int[2] array
+    var range = cells[0].split("..");
+    var start = parseInt(range[0], 16);
+    var end = parseInt(range[1] || range[0], 16);
+    cells[0] = [start, end];
+    
+    if (cells[2]) {
+      // parse replacement to int[] array
+      var replacement = cells[2].split(" ");
+      if (replacement[0] === "") { // empty array
+        replacement = [];
+      }
+      
+      replacement = replacement.map(function(r) {
+        return parseInt(r, 16);
+      });
+      
+      cells[2] = replacement;
+    }
+    
+    lines.push(cells);
+  });
+  
+  // we could drop valid chars, but those are only ~1000 ranges and
+  // binary search is way to quick to even notice that
+  
+  fs.writeFile(__dirname + "/../lib/mapping_table.json", JSON.stringify(lines));
+});
