ee849927d703e4b510eb3f1e1de5cac99a07e96b
Domenic Denicola <d@domenic.me>
2017-01-07 21:12:31 -0500

    Add linting and update to conform
    This includes a potentially-important bugfix where the undefined variable `processing` was being referenced (should be processing_option/processingOption).

--- scripts/generateMappingTable.js
+++ scripts/generateMappingTable.js
@@ -1,47 +1,50 @@
 "use strict";
 
-var fs = require("fs");
-var request = require("request");
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
 
-request.get("http://www.unicode.org/Public/idna/latest/IdnaMappingTable.txt", function(err, res, body) {
+request.get("http://www.unicode.org/Public/idna/latest/IdnaMappingTable.txt", (err, res, body) => {
   if (err) {
     throw err;
   }
-  
-  var lines = [];
-  
-  body.split("\n").forEach(function(l) {
-    l = l.split("#")[0]; // remove comments
-    var cells = l.split(";").map(function(c) {
+
+  const lines = [];
+
+  body.split("\n").forEach(l => {
+    l = l.split("#")[0]; // Remove comments
+    const cells = l.split(";").map(c => {
       return c.trim();
     });
-    if (cells.length === 1) return;
-    
-    // parse ranges to int[2] array
-    var range = cells[0].split("..");
-    var start = parseInt(range[0], 16);
-    var end = parseInt(range[1] || range[0], 16);
+    if (cells.length === 1) {
+      return;
+    }
+
+    // Parse ranges to int[2] array
+    const range = cells[0].split("..");
+    const start = parseInt(range[0], 16);
+    const end = parseInt(range[1] || range[0], 16);
     cells[0] = [start, end];
-    
+
     if (cells[2] !== undefined) {
-      // parse replacement to int[] array
-      var replacement = cells[2].split(" ");
-      if (replacement[0] === "") { // empty array
+      // Parse replacement to int[] array
+      let replacement = cells[2].split(" ");
+      if (replacement[0] === "") { // Empty array
         replacement = [];
       }
-      
-      replacement = replacement.map(function(r) {
+
+      replacement = replacement.map(r => {
         return parseInt(r, 16);
       });
-      
+
       cells[2] = replacement;
     }
-    
+
     lines.push(cells);
   });
-  
-  // we could drop valid chars, but those are only ~1000 ranges and
+
+  // We could drop valid chars, but those are only ~1000 ranges and
   // binary search is way to quick to even notice that
-  
-  fs.writeFile(__dirname + "/../lib/mappingTable.json", JSON.stringify(lines));
+
+  fs.writeFile(path.resolve(__dirname, "../lib/mappingTable.json"), JSON.stringify(lines));
 });
--- scripts/getLatestUnicodeTests.js
+++ scripts/getLatestUnicodeTests.js
@@ -1,12 +1,14 @@
 "use strict";
+/* eslint-disable no-process-env, no-process-exit */
 
 if (process.env.NO_UPDATE) {
   process.exit(0);
 }
 
-var fs = require("fs");
-var request = require("request");
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
 
-var target = fs.createWriteStream(__dirname + "/../test/unicode/IdnaTest.txt");
+const target = fs.createWriteStream(path.resolve(__dirname, "../test/unicode/IdnaTest.txt"));
 request.get("http://www.unicode.org/Public/idna/latest/IdnaTest.txt")
   .pipe(target);
