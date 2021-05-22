fb2368d67a02195a6a8ca6df81e930826c66ea8b
Timothy Gu <timothygu99@gmail.com>
2021-05-22 03:03:28 -0700

    Replace pump with stream.pipeline

--- scripts/getLatestTests.js
+++ scripts/getLatestTests.js
@@ -8,24 +8,24 @@ if (process.env.NO_UPDATE) {
 const fs = require("fs");
 const path = require("path");
 const { promisify } = require("util");
-// Replace this with stream.pipeline when we require Node.js 10.x.
-const pump = require("pump");
+// Replace this with stream/promises.pipeline when we require Node.js 15.x.
+const { pipeline } = require("stream");
 const fetch = require("node-fetch");
 const { unicodeVersion } = require("../package.json");
 
-const pumpPromise = promisify(pump);
+const pipelinePromise = promisify(pipeline);
 
 async function main() {
   await Promise.all([
     (async () => {
       const target = fs.createWriteStream(path.resolve(__dirname, "../test/fixtures/IdnaTestV2.txt"));
       const response = await fetch(`https://unicode.org/Public/idna/${unicodeVersion}/IdnaTestV2.txt`);
-      await pumpPromise(response.body, target);
+      await pipelinePromise(response.body, target);
     })(),
     (async () => {
       const asciiTarget = fs.createWriteStream(path.resolve(__dirname, "../test/fixtures/toascii.json"));
-      const response = await fetch("https://raw.githubusercontent.com/web-platform-tests/wpt/112ad5ca55d55f6da2ccc7468e6dcc91b4e5d223/url/resources/toascii.json");
-      await pumpPromise(response.body, asciiTarget);
+      const response = await fetch("https://github.com/web-platform-tests/wpt/raw/112ad5ca55d55f6da2ccc7468e6dcc91b4e5d223/url/resources/toascii.json");
+      await pipelinePromise(response.body, asciiTarget);
     })()
   ]);
 }
